9e3b8812ebf0e84f10cdb348ca848208e2a34f45
yoojh9 <yoojh9@gmail.com>
2019-11-06 13:41:18 +0900

    Collections > Set 추가

--- 05.Collections, JCF & Function Basics/README.md
+++ 05.Collections, JCF & Function Basics/README.md
@@ -1,8 +1,28 @@
 ## 1. Collections - Set
-#### 1. Set
+## 1. Set
 ```
  interface Set<out E> : Collection<E>(source)
 ```
  - 일반적으로 Set은 read-only
  - MutableSet은 추가하거나 삭제 가능하다.
 
+#### 1. Data Structure
+ - 공통 데이터 구조, Set, List, Hash; kotlin.collections.* (JVM JCF)
+
+#### 2. Set
+ - 데이터 구조; 중복되지 않는 데이터들을 단순히 묶은 집합
+    - unordered: 일반적으로 순서가 없고 (요소 추가 시 순서가 보존 안됨)
+    - no duplicated: 포함되는 요소(element)의 중복을 허용하지 않음
+
+#### 3. interface Set
+ - Set<out E>, setOf(), isEmpty(), size, in, contains()
+
+#### 4. Mutability
+ - MutableSet<E>, mutableSetOf(), add(), addAll(), remove(), clear()..
+
+#### 5. Iteration
+ - 요소들을 루프로 탐색할 수 있음
+ - iterator(), hasNext(), next() => "xyz".toSet()
+
+#### 6. operations
+ - containsAll(), intersect, union, subtract
--- 05.Collections, JCF & Function Basics/SetProject/src/main/SetMain.kt
+++ 05.Collections, JCF & Function Basics/SetProject/src/main/SetMain.kt
@@ -32,12 +32,48 @@ fun main(args: Array<String>) {
     /**
      * iteration, iterator - 요소들을 순차적으로 탐색
      */
-    val myAlphabets = setOf<Char>('A','B','C','D', 69.toChar(), 'F', 'G')
+    // val myAlphabets = setOf<Char>('A','B','C','D', 69.toChar(), 'F', 'G')
+    val myAlphabets = "ABCDEFG".toSet()
     val it = myAlphabets.iterator()
     while(it.hasNext()) {
         print("${it.next()} \t")
     }
 
+    /**
+     * Set 연산
+     */
+    val myName = "BEN".toSet()  // Set<Char>
+    val yourName = "DEA".toSet()
+    val hisName = "XYZ".toSet()
+
+    // 1. 포함관계
+    println()
+    println("myAlphabets.containsAll(myName) = ${myAlphabets.containsAll(myName)}")
+    println("myAlphabets.containsAll(yourName) = ${myAlphabets.containsAll(yourName)}")
+
+    // 2. 부분집합 (교집합)
+    println("myAlphabets intersect myName = ${myAlphabets intersect  myName}")
+    println("myAlphabets intersect yourName = ${myAlphabets intersect  yourName}")
+
+    // 3. 합집합
+    println("myAlphabets union myName = ${myAlphabets union myName}")
+    println("myAlphabets + myName = ${myAlphabets + myName}")
+
+    // 4. 차집합
+    println("myAlphabets subtract myName = ${myAlphabets subtract myName}")
+    println("myAlphabets - myName = ${myAlphabets - myName}")
 
 
+    /**
+     * 불변 vs 가변
+     */
+    val herName = setOf('M','a','r','y')
+    // herName.add('n') // error: 불변 immutable
+
+    val herName2 : MutableSet<Char> = mutableSetOf('M','a','r','y')    //
+    herName2.add('n')   // 가변 mutable
+    herName2.addAll(setOf('L','e'))
+    herName2.remove('y')
+    herName2.clear()
+    println("herName2 = ${herName2}")
 }
